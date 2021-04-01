a1adad3171bd8b8edd3beeaba3ab7559b1b5b2cc
Raju Kattamanchi <raju.kattamanchi@in.abb.com>
2021-04-01 15:45:56 +0530

    test2

--- Logging/Source/Logger.cs
+++ Logging/Source/Logger.cs
@@ -260,7 +260,7 @@ namespace Common.Logging
         #endregion
 
         /// <summary>
-        /// Tracks the event.
+        /// Tracks the events.
         /// </summary>
         /// <param name="eventName">Name of the event.</param>
         /// <param name="properties">The properties.</param>
